3e05ec1345953a455c6d8d4c61c1ad97fbd41e88
Nitya Jain <nityaprofessional6402@gmail.com>
2025-04-12 14:47:27 +0530

    chore: Update npm dependencies and add PocketBase integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import PocketBase from 'pocketbase';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [record, setRecord] = useState([])
+  const pb = new PocketBase('http://127.0.0.1:8090');
+
+  useEffect(() => {
+    try{
+    const fetchData = async () => {
+      const records = await pb.collection('categories').getFullList();
+      // console.log(records);
+      setRecord(records);
+    }
+
+    fetchData()
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+  },[])
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Color</th>
+        </tr>
+      </thead>
+      <tbody>
+        {record.map((item) => (
+          <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+            <td style={{ backgroundColor: item.color }}>{item.color}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+      
     </>
   )
 }
